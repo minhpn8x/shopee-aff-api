e7a7e440d1d7d53ec9e5b4dd564b4d7cb2400472
minhpn8x <minhpn89@gmail.com>
2026-08-28 18:00:11 +0700

    Update types.ts

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,11 +1,12 @@
 import type { OpenAPIHono, RouteConfig, RouteHandler } from '@hono/zod-openapi'
 
 export interface AppBindings {
-  Bindings: {
-    NODE_ENV?: string
-    SERVER_URL?: string
-    SHOPEE_BASE_API?: string
-  }
+Bindings: {
+  NODE_ENV?: string
+  SERVER_URL?: string
+  SHOPEE_BASE_API?: string
+  SHOPEE_COOKIE?: string
+}
 }
 
 export type AppOpenAPI = OpenAPIHono<AppBindings>
